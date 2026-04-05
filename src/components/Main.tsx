3f9f1ba583a88015ad2583236b9f94e8b12ee8b4
Harsh Yadav <harshyadav5939@gmail.com>
2026-04-05 21:40:35 +0530

    first commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import '../assets/styles/Main.scss';
+import prestige from "../assets/images/prestige.jpeg";
+
+function Main() {
+
+  return (
+    <div className="container">
+      <div className="about-section">
+        <div className="image-wrapper">
+         <div className="image-wrapper">
+  <img src={prestige} alt="Avatar" className="profile-img" />
+</div>
+        </div>
+        <div className="content">
+          <div className="social_icons">
+            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+          <h1>Ugochiyere Marvellous Isaiah</h1>
+          <p>Software Engineer</p>
+
+          <div className="mobile_social_icons">
+            <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
