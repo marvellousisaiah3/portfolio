a373995ecfe953badb21ace2357798370cd271bb
marvellousisaiah3 <marvellousisaiah3@gmail.com>
2026-04-19 20:25:31 +0530

    added projects and other details

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,6 +9,7 @@ import mock07 from '../assets/images/mock07.png';
 import mock08 from '../assets/images/mock08.png';
 import mock09 from '../assets/images/mock09.png';
 import mock10 from '../assets/images/mock10.png';
+import foodgo from '../assets/images/food-go-1.jpg'
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -17,11 +18,11 @@ function Project() {
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Filmate AI</h2></a>
-                <p>Developed movie finder app with semantic search and sentiment analysis using OpenAI GPT-3.5 Turbo, Qdrant, React, and Flask.</p>
+                <a href="https://github.com/marvellousisaiah3/food-go-flutter" target="_blank" rel="noreferrer"><img src={foodgo} className="zoom" alt="thumbnail" height="600" width="100%"/></a>
+                <a href="https://github.com/marvellousisaiah3/food-go-flutter" target="_blank" rel="noreferrer"><h2>Flutter Food Go</h2></a>
+                <p>A comprehensive Flutter-based food delivery app with Firebase backend, featuring seamless food ordering, real-time order tracking, and integrated payment solutions. Users can browse categorized food items, place orders, track delivery status, and manage their wallet balance.</p>
             </div>
-            <div className="project">
+            {/* <div className="project">
                 <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><h2>High Speed Chase</h2></a>
                 <p>Designed, developed, and launched a 3D multiplayer racing game with C# and Unity. This is available on Itch.io for gamers worldwide to enjoy.</p>
@@ -65,7 +66,7 @@ function Project() {
                 <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://github.com/yujisatojr/submeowrine" target="_blank" rel="noreferrer"><h2>Submeowrine</h2></a>
                 <p>Developed and released an Android mobile application using Java and Android Studio that runs a 2D shooting game.</p>
-            </div>
+            </div> */}
         </div>
     </div>
     );
